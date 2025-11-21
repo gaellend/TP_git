9cb516e4d7e38e59ee08bcf9a48fca812f3e763c
shugarin <gaelle.industri@gmail.com>
2025-11-21 11:58:39 +0100

    Ajout commentaire

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    //Ceci est un commentaire de Gaëlle Industri
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
