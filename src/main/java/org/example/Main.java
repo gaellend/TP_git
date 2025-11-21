69140217c21b20bc2371965a8af187272ba0eb86
Nicolas Linck <nicolas.linck@etud.univ-jfc.fr>
2025-11-21 11:57:54 +0100

    Ajout commentaire

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    // Ceci est un commentaire de Nicolas Linck
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
